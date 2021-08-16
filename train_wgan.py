26cb9340bced9bcea2c5341ad0d4ad9aef6bd4e7
ProgCat <progcat@protonmail.com>
2021-08-16 19:22:33 +0800

    add loading mechanism to train_wgan.py

--- train_wgan.py
+++ train_wgan.py
@@ -2,7 +2,7 @@
 #
 # 產生圖片大小: 64x64
 #
-
+from os import path
 import torch
 import torch.nn as nn
 import torch.optim as optimizers
@@ -16,9 +16,12 @@ import matplotlib.pyplot as plt
 
 from wgan import NEPPDiscriminator, NEPPGenerator
 
-# manualSeed = 666
-# random.seed(manualSeed)
-# torch.manual_seed(manualSeed)
+G_NET_PATH = 'wgan_models/g_net.pth'
+D_NET_PATH = 'wgan_models/d_net.pth'
+
+manualSeed = 666
+random.seed(manualSeed)
+torch.manual_seed(manualSeed)
 
 # from: https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html
 def weights_init(m):
@@ -31,12 +34,21 @@ def weights_init(m):
 
 ## 定義生成網路
 g_net = NEPPGenerator().cuda()
-g_net.apply(weights_init)
+if path.exists(G_NET_PATH):
+    print(f'Load generator from {G_NET_PATH}')
+    g_net.load_state_dict(torch.load(G_NET_PATH))
+else:
+    g_net.apply(weights_init)
 print(g_net)
 
 ## 定義評論網路
 d_net = NEPPDiscriminator().cuda()
-d_net.apply(weights_init)
+if path.exists(D_NET_PATH):
+    print(f'Load discriminator from {D_NET_PATH}')
+    d_net.load_state_dict(torch.load(D_NET_PATH))
+else:
+    d_net.apply(weights_init)
+print(d_net)
 
 ## 準備訓練資料
 puipui_imgs = ImageFolder(root='puipui', transform=transforms.Compose([
@@ -52,7 +64,7 @@ print('number of images:', len(puipui_imgs.imgs))
 ## 超參數
 EPOCHS = 50000
 BATCH_SIZE = 64
-LR = 0.0001
+LR = 0.001
 C = 0.01
 UPDATE_G_PER_EPOCH = 5
 
@@ -106,7 +118,7 @@ for epoch in range(1, EPOCHS+1):
             generated_puipui = g_net(fixed_noise).detach().cpu()
             img = make_grid(generated_puipui, padding=2, normalize=True)
         plt.imsave(f'wgan_results/{epoch}.jpg', np.transpose(img,(1,2,0)).numpy())
-        torch.save(g_net.state_dict(), 'wgan_models/g_net.pth')
-        torch.save(d_net.state_dict(), 'wgan_models/d_net.pth')
+        torch.save(g_net.state_dict(), G_NET_PATH)
+        torch.save(d_net.state_dict(), D_NET_PATH)
 
 
